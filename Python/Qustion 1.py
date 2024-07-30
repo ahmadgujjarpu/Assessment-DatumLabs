67f3451446130bd3804aaad256dbb40a5fcf1427
ahmadgujjarpu <95549131+ahmadgujjarpu@users.noreply.github.com>
2024-07-30 15:10:06 +0500

    Update and rename test to Qustion 1.py

---
+++ Python/Qustion 1.py
@@ -0,0 +1,19 @@
+# Deined input array
+RawNmubers=[1,1,"None","None",2,"None","None"]
+#Defined Output array
+NumbersList=[]
+#Defined Number variable to store last not none value
+lastnumber=0
+# Loop to go through all the values of raw list
+for i in RawNmubers:
+    # Condition to check non None value
+    if(i!="None"):
+        # Append the for item into new list
+        NumbersList.append(i)
+        #Set the current value to this variable
+        lastnumber=i
+    else:
+        # Append lastnumber value to list
+        NumbersList.append(lastnumber)
+# print new list
+print(NumbersList)
